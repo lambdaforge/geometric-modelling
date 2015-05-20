a607528c4afe646f7eb4acee8c120938aeef553a
kordano <konrad.kuehne@rocketmail.com>
2015-05-20 10:33:16 +0200

    add assignment 4

---
+++ ws/assignment4.clj
@@ -0,0 +1,42 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Assignment 4
+;; **
+
+;; @@
+(ns barren-winter
+  (:refer-clojure :exclude [* - + == /])
+  (:require [gorilla-plot.core :as plot]
+            [gorilla-repl.html :as html]
+            [gg4clj.core :as gg4clj]
+            [geometric-modelling.core :as gma]
+            [clojure.core.matrix :refer :all]
+            [clojure.core.matrix.operators :refer :all]))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
+;; <=
+
+;; @@
+(set-current-implementation :vectorz)
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-keyword'>:vectorz</span>","value":":vectorz"}
+;; <=
+
+;; **
+;;; ## Algorithm of de Boor
+;; **
+
+;; @@
+
+;; @@
+
+;; **
+;;; ## Rational Bézier Curves
+;; **
+
+;; @@
+
+;; @@
