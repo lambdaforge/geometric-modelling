6e1ab4843b315fec993594767af448ffbdd901a3
kordano <konrad.kuehne@rocketmail.com>
2015-06-02 10:03:04 +0200

    add quil dep

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
                  [net.mikera/core.matrix "0.34.0"]
                  [net.mikera/vectorz-clj "0.29.0"]
                  [gg4clj "0.1.0"]
+                 [quil "2.2.5"]
                  [incanter "1.9.0"]]
   :plugins [[lein-gorilla "0.3.4"]])
